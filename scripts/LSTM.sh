ba5a4d6561bc9f7c7e10c3ce16594261bf8d4b89
msc5 <19colemanm@gmail.com>
2022-04-20 00:46:38 -0400

    misc

--- scripts/LSTM.sh
+++
@@ -1,6 +0,0 @@
-
-python -m src train LSTM GeneratedSins --num_layers 1 --max_epochs 2
-python -m src train LSTM GeneratedSins --num_layers 2 --max_epochs 2
-
-python -m src train LSTM GeneratedNoise --num_layers 1 --max_epochs 2
-python -m src train LSTM GeneratedNoise --num_layers 2 --max_epochs 2
